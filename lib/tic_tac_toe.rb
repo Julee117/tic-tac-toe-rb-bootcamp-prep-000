f149717ad36ec266f035610da21bbf92f293a217
Julianne Kim <juliannne@gmail.com>
2017-10-11 01:37:42 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -60,9 +60,9 @@ def full?(board)
 end
 
 def draw?(board)
-  if !won?(board) && full?(board) 
-    return true 
-  else 
+  if !won?(board) && full?(board)
+    return true
+  else
     return false
   end
 end

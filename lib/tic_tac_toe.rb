2d6aaee292c42cb76ee457127733b035833023cb
Julianne Kim <juliannne@gmail.com>
2017-10-11 01:32:30 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,7 +49,7 @@ def won?(board)
   WIN_COMBINATIONS.any? do |combo|
     if (board[combo][0] == "X" && board[combo][1] == "X" && board[combo][2] == "X") ||
       (board[combo][0] == "O" && board[combo][1] == "O" && board[combo][2] == "O")
-      return combo 
+      return combo
     end 
-  end 
+  end
 end
